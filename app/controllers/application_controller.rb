5a01f75487380c2f12f424c44717e8f3586b3594
tovawieds <wiedermanntova@gmail.com>
2025-06-25 20:29:59 -0400

    Add more code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   def index
-    render json: {message: "HI! How's everyone's day going?"}
+    render json: { message: "HI! How's everyone's day going? Hi everyone!" }
   end
   allow_browser versions: :modern
 end
