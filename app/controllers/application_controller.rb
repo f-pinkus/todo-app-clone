9ed277f9a267e27605be5f2841be42f0be3bf291
f-pinkus <faigypinkus@gmail.com>
2025-06-25 20:25:01 -0400

    new code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   def index
-    render json: {message: "HI!"}
+    render json: {message: "HI! How's everyone's day going?"}
   end
   allow_browser versions: :modern
 end
